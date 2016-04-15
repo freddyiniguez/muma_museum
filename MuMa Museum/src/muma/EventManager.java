db7a6759cfb73581c9133b5bf7b8276bdec36eea
Freddy Íñiguez López <iniguez.devt@gmail.com>
2016-04-15 15:21:40 -0500

    Connection to RabbitMQ

--- MuMa Museum/src/muma/EventManager.java
+++ MuMa Museum/src/muma/EventManager.java
@@ -1,5 +1,55 @@
 package muma;
 
-public class EventManager {
+/**
+ * @author Choreros
+ * @class EventManager.java
+ * @version 0.1
+ * @description This class's purpose is to manage the messages between the different devices connected to MuMa Software. 
+ * It implements a RabbitMQ's queue to store the messages and to retrieve these ones when devices are ready to pull information.
+ */
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+public class EventManager {
+	// Variables
+	private final static String QUEUE_NAME = "muma";
+	
+	
+	/**
+	 * @method Constructor
+	 * @description It creates the EventManager responsible for communicate MuMa components.
+	 */
+	public EventManager(){
+		if (ConnectToRabbit())
+			System.out.println(">>> SUCCESS! The connection with RabbitMQ was successfully.");
+	}
+	
+	/**
+	 * @method ConnectToRabbit()
+	 * @return True is connection was successful. False otherwise.
+	 */
+	private boolean ConnectToRabbit(){
+		try{
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			channel.queueDeclare();
+			return true;
+		}catch(Exception e){
+			System.out.println(">>> WARNING! We have encounter errors when trying to connect to RabbitMQ: " + e.getMessage());
+			return false;
+		}
+	}
 }
