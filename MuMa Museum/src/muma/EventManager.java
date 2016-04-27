361df67033ead4e59b59ad89e6f0480e81898b42
Freddy Íñiguez López <iniguez.devt@gmail.com>
2016-04-27 01:37:44 -0500

    Methods to send and receive messages were written.

--- MuMa Museum/src/muma/EventManager.java
+++ MuMa Museum/src/muma/EventManager.java
@@ -21,20 +21,35 @@ import com.rabbitmq.client.Envelope;
 
 import javax.swing.*;
 import java.awt.Desktop;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class EventManager extends JFrame implements Runnable, ActionListener{
-	// Variables
-	private final static String QUEUE_NAME = "muma";
+	private static final String QUEUE_NAME = "muma";
+	private static final String SENSOR_TEMPERATURE_ID = "-5";
+	private static final String SENSOR_HUMIDITY_ID = "-4";
+	private static final String CONTROLLER_TEMPERATURE_ID = "5";
+	private static final String CONTROLLER_HUMIDITY_ID = "4";
+	private static final String CHANGE_TEMPERATURE_ID = "CT";
+	private static final String CHANGE_HUMIDITY_ID = "CH";
 	private static final long serialVersionUID = 1L;
+	
 	private JMenuBar jmbTopBar;
 	private JMenu jmFile;
 	private JMenu jmHelp;
 	private JMenuItem jmiExit;
 	private JMenuItem jmiAbout;
 	private JMenuItem jmiUserManual;
+	private JLabel jlTemperature;
+	private JTextField jtfTemperature;
+	private JButton jbTemperature;
+	private JLabel jlHumidity;
+	private JTextField jtfHumidity;
+	private JButton jbHumidity;
 	
 	/**
 	 * @method Constructor
@@ -86,18 +101,36 @@ public class EventManager extends JFrame implements Runnable, ActionListener{
 		jmHelp.addSeparator();
 		jmHelp.add(jmiAbout);
 		jmbTopBar.add(jmHelp);
-		this.setJMenuBar(jmbTopBar);
 		
+		this.setJMenuBar(jmbTopBar);
 		this.setTitle(windowTitle);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600,600);
+		this.setLayout(new GridLayout(2,3));
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
+		
+		jlTemperature = new JLabel("Temperature");
+		jtfTemperature = new JTextField("Temperature");
+		jbTemperature = new JButton("Change Temperature");
+		jbTemperature.addActionListener(this);
+		this.add(jlTemperature);
+		this.add(jtfTemperature);
+		this.add(jbTemperature);
+		
+		jlHumidity = new JLabel("Humidity");
+		jtfHumidity = new JTextField("Humidity");
+		jbHumidity = new JButton("Change Humidity");
+		jbHumidity.addActionListener(this);
+		this.add(jlHumidity);
+		this.add(jtfHumidity);
+		this.add(jbHumidity);
+		
 		this.setVisible(true);
 	}
 	
 	/**
-	 * @method ConnectToRabbit()
+	 * @method connectToRabbit
 	 * @return True is connection was successful. False otherwise.
 	 */
 	private boolean connectToRabbit(){
@@ -113,15 +146,98 @@ public class EventManager extends JFrame implements Runnable, ActionListener{
 			return false;
 		}
 	}
+	
+	/**
+	 * @method sendMessage
+	 * @parameter Receives two parameter. The first one is an identifier to the action to take. The second one is a message.
+	 * @return True is the message was correctly sent.
+	 */
+	private boolean sendMessage(String CHANNEL_SEND_ID, String message){
+		try{
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			// Sends the message
+			channel.exchangeDeclare(CHANNEL_SEND_ID, "fanout");
+			channel.basicPublish(CHANNEL_SEND_ID, "", null, message.getBytes("UTF-8"));
+			channel.close();
+			connection.close();
+			return true;
+		}catch(IOException|TimeoutException e){
+			System.out.println(">>> ERROR! The Message could not be delivered: \n" + e.getMessage());
+			return false;
+		}
+	}
+	
+	/**
+	 * @method receiveMessageFromTemperatureSensor
+	 * @description Receive a message from the Temperature sensor.
+	 */
+	private void receiveMessageFromTemperatureSensor(){
+		try{
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			// Receives the message
+			channel.exchangeDeclare(SENSOR_TEMPERATURE_ID, "fanout");
+			channel.queueBind(QUEUE_NAME, SENSOR_TEMPERATURE_ID, "");
+			// String queueName = channel.queueDeclare().getQueue(); When you unknown the queue name.
+			Consumer consumer;
+			consumer = new DefaultConsumer(channel){
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException{
+					System.out.println(">>> INFO! I received a message from the Temperature sensor: " + new String(body, "UTF-8"));
+				}
+			};
+			channel.basicConsume(QUEUE_NAME, true, consumer);
+			channel.close();
+			connection.close();
+		}catch(IOException|TimeoutException e){
+			System.out.println(">>> ERROR! The Message could not be received: \n" + e.getMessage());
+		}
+	}
+	
+	/**
+	 * @method receiveMessageFromHumiditySensor
+	 * @description Receive a message from the Humidity sensor.
+	 */
+	private void receiveMessageFromHumiditySensor(){
+		try{
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			// Receives the message
+			channel.exchangeDeclare(SENSOR_HUMIDITY_ID, "fanout");
+			channel.queueBind(QUEUE_NAME, SENSOR_HUMIDITY_ID, "");
+			// String queueName = channel.queueDeclare().getQueue(); When you unknown the queue name.
+			Consumer consumer;
+			consumer = new DefaultConsumer(channel){
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException{
+					System.out.println(">>> INFO! I received a message from the Humidity sensor: " + new String(body, "UTF-8"));
+				}
+			};
+			channel.basicConsume(QUEUE_NAME, true, consumer);
+			channel.close();
+			connection.close();
+		}catch(IOException|TimeoutException e){
+			System.out.println(">>> ERROR! The Message could not be received: \n" + e.getMessage());
+		}
+	}
 
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.out.println(">>> INFO! MuMa Software is running.");
+		receiveMessageFromTemperatureSensor();
+		receiveMessageFromHumiditySensor();
 	}
 	
 	@Override
@@ -139,6 +255,24 @@ public class EventManager extends JFrame implements Runnable, ActionListener{
 			} catch (Exception e1) {
 				e1.printStackTrace();
 				JOptionPane.showMessageDialog(null, "The User's Manual could not be found.", "File not found.", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
+		// Change Temperature
+		if (e.getSource()==jbTemperature){
+			if (sendMessage(CHANGE_TEMPERATURE_ID, jtfTemperature.getText())){
+				System.out.println(">>> INFO! The temperature has changed.");
+			}else{
+				System.out.println(">>> ERROR! There was an error changing the temperature.");
+			}
+		}
+		
+		// Change Humidity
+		if (e.getSource()==jbHumidity){
+			if(sendMessage(CONTROLLER_HUMIDITY_ID, jtfHumidity.getText())){
+				System.out.println(">>> INFO! The humidity has changed.");
+			}else{
+				System.out.println(">>> ERROR! There was an error changing the humidity");
 			}
 		}
 	}
