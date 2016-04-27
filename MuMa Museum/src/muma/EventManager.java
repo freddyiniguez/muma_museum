568373bf7b2299a278ff82784b4e6e137d88bfd6
Freddy Íñiguez López <iniguez.devt@gmail.com>
2016-04-26 23:37:15 -0500

    Creating the new frame to the EventManager.

--- MuMa Museum/src/muma/EventManager.java
+++ MuMa Museum/src/muma/EventManager.java
@@ -18,20 +18,74 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-
+import javax.swing.*;
 import java.io.IOException;
 
-public class EventManager {
+public class EventManager extends JFrame implements Runnable{
 	// Variables
 	private final static String QUEUE_NAME = "muma";
+	private static final long serialVersionUID = 1L;
+	private JMenuBar jmbTopBar;
+	private JMenu jmFile;
+	private JMenu jmHelp;
+	private JMenuItem jmiExit;
+	private JMenuItem jmiAbout;
+	private JMenuItem jmiUserManual;
 	
 	/**
 	 * @method Constructor
 	 * @description It creates the EventManager responsible for communicate MuMa components.
 	 */
 	public EventManager(){
-		if (connectToRabbit())
+		if (connectToRabbit()){
 			System.out.println(">>> SUCCESS! The connection with RabbitMQ was successfully.");
+		}else{
+			JOptionPane.showMessageDialog(null, "In order to execute MuMa Software, you must start RabbitMQ first.", "Error initializing MuMa Software", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * @method Constructor
+	 * @description It creates the EventManager responsible for communicate MuMa components.
+	 * @parameter Receives the name of the window.
+	 */
+	public EventManager(String windowTitle){
+		if (connectToRabbit()){
+			System.out.println(">>> SUCCESS! The connection with RabbitMQ was successfully.");
+		}else{
+			JOptionPane.showMessageDialog(null, "In order to execute MuMa Software, you must start RabbitMQ first.", "Error initializing MuMa Software", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		createsWindow(windowTitle);
+	}
+	
+	/**
+	 * @method createsWindow
+	 * @description It creates the EventManager responsible for communicate MuMa components.
+	 * @parameter Receives the title of the window.
+	 */
+	private void createsWindow(String windowTitle){
+		jmbTopBar = new JMenuBar();
+		jmFile = new JMenu("File");
+		jmiExit = new JMenuItem("Exit");
+		jmFile.add(jmiExit);
+		jmbTopBar.add(jmFile);
+		
+		jmHelp = new JMenu("Help");
+		jmiAbout = new JMenuItem("About Muma");
+		jmiUserManual = new JMenuItem("See User Manual");
+		jmHelp.add(jmiUserManual);
+		jmHelp.addSeparator();
+		jmHelp.add(jmiAbout);
+		jmbTopBar.add(jmHelp);
+		
+		this.setTitle(windowTitle);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600,600);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 	
 	/**
@@ -51,11 +105,22 @@ public class EventManager {
 			return false;
 		}
 	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(">>> INFO! MuMa Software is runing.");
+	}
 	
 	/**
 	 * @method main
 	 */
 	public static void main(String[] args){
-		EventManager muma = new EventManager();
+		EventManager muma = new EventManager("MuMa Museum: Security Software System");
+		muma.run();
 	}
 }
